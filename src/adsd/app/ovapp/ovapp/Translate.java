2b1a0cdb4fd9aa8db77326e135709f76456e7571
Joel Ipenburg <joel.ipenburg@student.hu.nl>
2020-05-26 14:02:51 +0200

    Made a separate class for the translate methods.

---
+++ src/adsd/app/ovapp/ovapp/Translate.java
@@ -0,0 +1,63 @@
+package adsd.app.ovapp.ovapp;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Translate {
+
+    private Map languageMap;
+    protected String Language;
+
+    public Translate(String language) 
+    {
+        Language = language;
+        languageMap = new HashMap();
+        FillLanguageMap();
+    }
+    //puts a new value into the language list.
+    private void FillLanguageMap() 
+    {
+        //profile
+        languageMap.put("Wijzig profiel", "Edit profile");
+        languageMap.put("Favorieten:", "Favorites:");
+        languageMap.put("Opgeslagen:", "Saved:");
+        languageMap.put("Herinneringen:", "Reminders:");
+        languageMap.put("Mijn beschrijving:", "My description:");
+        languageMap.put("Kaart:", "Map:");
+        languageMap.put("Naam:", "Name:");
+        languageMap.put("Achternaam:", "Surname:");
+        languageMap.put("Leeftijd:", "Age:");
+        languageMap.put("Stad:", "City:");
+        languageMap.put("Straatnaam:", "Streetname:");
+        //languageMap.put("English", "Nederlands");
+        languageMap.put("EN", "NL");
+
+        //TravelPlanner
+        languageMap.put("Aankomst:", "Destination:");
+        languageMap.put("Vertrek:", "Departure:");
+        languageMap.put("Nu", "Now");
+        languageMap.put("Zoeken", "Search");
+        //Location
+        languageMap.put("Bestemming:", "Destination:");
+        languageMap.put("Vervoerstype:", "Transport type:");
+        languageMap.put("Wijzig reis", "Change trip");
+
+    }
+    //gets the translated word from languagemap and returns the word.
+    public String TransLang(String word) 
+    {
+
+        if(Language == "EN")
+        {
+            if((String)languageMap.get(word) != null)
+            {
+                return (String)languageMap.get(word);
+            }
+        }
+
+        return word;
+
+    }
+
+
+}
