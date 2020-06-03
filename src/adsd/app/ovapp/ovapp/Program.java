f4a88a00404495746af1cf6fabb4e0c6ea8ae538
Raymond Schonewille <raymond.schonewille@student.hu.nl>
2020-06-03 17:41:37 +0200

    fix for OvApp en Program

--- src/adsd/app/ovapp/ovapp/Program.java
+++ src/adsd/app/ovapp/ovapp/Program.java
@@ -1,30 +1,25 @@
 package adsd.app.ovapp.ovapp;
 
 import java.awt.EventQueue;
-import java.awt.Window;
 
-import adsd.app.ovapp.train.TrainDataModel;
-import adsd.app.ovapp.train.TrainTime;
-
-public class Program 
+public class Program
 {
-	static OvApp  ovapp;
-
-public static void main(String[] args) 
-{
-	EventQueue.invokeLater(new Runnable() 
-	{
-		public void run() 
-		{
-			try 
-			{
-				OvApp nw = new OvApp();	
-				nw.NewScreen(); // om te verwijzen naar ovapp met gui moet er een newscreen aan gemaakt worden
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-	});
-}}
+    public static void main(String[] args)
+    {
+        EventQueue.invokeLater(new Runnable()
+        {
+            public void run()
+            {
+                try
+                {
+                    //newscreen is a alias for OvApp, here is Ovapp opened as a new main program
+                    new OvApp().newScreen();
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
