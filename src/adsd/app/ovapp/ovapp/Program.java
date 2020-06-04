a0d1e2ddb2c556cfe1e649dd2b6ffb5833ccabff
Raymond Schonewille <raymond.schonewille@student.hu.nl>
2020-06-04 16:17:31 +0200

    pulled from master

--- src/adsd/app/ovapp/ovapp/Program.java
+++ src/adsd/app/ovapp/ovapp/Program.java
@@ -4,24 +4,6 @@ import java.awt.EventQueue;
 
 public class Program
 {
-<<<<<<< src/adsd/app/ovapp/ovapp/Program.java
-	EventQueue.invokeLater(new Runnable() 
-	{
-		public void run() 
-		{
-			try 
-			{
-				OvApp nw = new OvApp();	
-				nw.newScreen(); // om te verwijzen naar ovapp met gui moet er een newscreen aan gemaakt worden
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-			}
-		}
-	});
-}}
-=======
     public static void main(String[] args)
     {
         EventQueue.invokeLater(new Runnable()
@@ -41,4 +23,3 @@ public class Program
         });
     }
 }
->>>>>>> src/adsd/app/ovapp/ovapp/Program.java
